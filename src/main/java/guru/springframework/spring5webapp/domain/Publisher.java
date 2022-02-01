c6bf0917ab289adab666448e2866bad7bf240a24
AndresAcosta <andres@pixelfreestudio.com>
2022-02-01 13:20:31 -0500

    added publisher entety per assigment 2

---
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -0,0 +1,98 @@
+package guru.springframework.spring5webapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String address_line1;
+    private String address_city;
+    private String address_state;
+    private String address_zip;
+
+    public Publisher() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address_line1='" + address_line1 + '\'' +
+                ", address_city='" + address_city + '\'' +
+                ", address_state='" + address_state + '\'' +
+                ", address_zip='" + address_zip + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress_line1() {
+        return address_line1;
+    }
+
+    public void setAddress_line1(String address_line1) {
+        this.address_line1 = address_line1;
+    }
+
+    public String getAddress_city() {
+        return address_city;
+    }
+
+    public void setAddress_city(String address_city) {
+        this.address_city = address_city;
+    }
+
+    public String getAddress_state() {
+        return address_state;
+    }
+
+    public void setAddress_state(String address_state) {
+        this.address_state = address_state;
+    }
+
+    public String getAddress_zip() {
+        return address_zip;
+    }
+
+    public void setAddress_zip(String address_zip) {
+        this.address_zip = address_zip;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Publisher publisher = (Publisher) o;
+
+        return id != null ? id.equals(publisher.id) : publisher.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
